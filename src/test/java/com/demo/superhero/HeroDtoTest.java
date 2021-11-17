205c1371d7edd4524cfa957133378a7d8ae693a6
Augusto Aragon <3626599-c-aragon1@users.noreply.gitlab.com>
2021-11-17 03:47:30 -0600

    change comment

--- src/test/java/com/demo/superhero/HeroDtoTest.java
+++ src/test/java/com/demo/superhero/HeroDtoTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 import com.demo.superhero.dto.HeroDto;
 
 public class HeroDtoTest {
-	
+
 	@Test
 	public void heroDtoTest() {
-		//Prepraring test
+		//Preparing test
 		HeroDto heroDto = new HeroDto();
 		heroDto.setId(1L);
 		heroDto.setName("Name");
-		
+
 		//Test
 		Assertions.assertEquals("Name", heroDto.getName());
 		Assertions.assertEquals(1L, heroDto.getId());
 	}
-	
+
 }
